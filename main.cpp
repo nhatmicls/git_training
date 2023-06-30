d7469f4f6219eb8041461625a5d9c1b6e973adf5
Micls <cptprice123@outlook.com>
2023-06-30 18:43:56 +0700

    [Micls_dev] Implenment new func

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "lib/compare/compare.h"
+#include "lib/math/math_func.h"
 
 int main()
 {
     std::cout << "hi\n";
+
+    int new_1 = plus(1, 2);
+    std::cout << "ANS: " << new_1 << "\n";
+
     return 0;
 }
